d4205524ed5ad84280d0abf963263389c15c9cf1
Sudip Kunda <sudipkunda@gmail.com>
2020-06-15 10:18:05 -0500

    Created autotex git folder and first commit

---
+++ helper.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+import pyautogui as pag
+import time
+
+theBrowser = [1700, 75]
+
+def delay():
+    time.sleep(1)
+
+def browserFocus(n):
+    for i in range(n): pag.hotkey('alt', 'tab')
+    delay()
+    pag.click(theBrowser)
